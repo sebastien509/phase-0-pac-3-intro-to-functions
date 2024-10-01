e55b6e9504317f7f1ab65a4c6ba8941a6173814c
Sebastien Fenelon <fenelonsebastien@outlook.com>
2024-10-01 11:26:26 -0500

    introduction to function completed under the index.js file.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Follow along with the examples here
+
+function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+  }
+
+  console.log("Hello", "Jean")
+  
+  function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+    console.log("I was called!");
+  }
+
+  
+console.log("Hi","Becka");
+console.log("I was called!");
